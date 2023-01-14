8f3459c32d7542a318024a1989c8559310382d53
balaji13028 <balaji13028@gmail.com>
2023-01-14 15:18:46 +0530

    changed api

--- jagbandhu_service_provider/lib/Constants/api.dart
+++ jagbandhu_service_provider/lib/Constants/api.dart
@@ -1,5 +1,6 @@
 class Apilink {
-  static const String ipAddress = 'http://192.168.1.23:3003';
+  // static const String ipAddress = 'http://192.168.1.23:3003';
   // static const String ipAddress = 'http://192.168.252.193:3003';
   // static const String ipAddress = 'http://192.168.1.5:3003';
+  static const String ipAddress = 'https://api.jagbandhu.com';
 }
